5f70da03fb67755bc6b1feb5c818ece65975e660
Temurbek Sabirov <prodota150@gmail.com>
2020-06-05 18:53:57 -0400

    Implement GifItem component

--- src/Components/GifItem.jsx
+++ src/Components/GifItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const GifItem = (image) => {
+const GifItem = ({ gif }) => {
+  const gifImageInfo = gif.images.fixed_height_small;
   return (
-    <li>
-      <img src={image.gif.url} />
-    </li>
+    <div className="ui segment">
+      <img
+        src={gifImageInfo.url}
+        alt={gifImageInfo.slug}
+        height={gifImageInfo.height}
+        width="125"
+      ></img>
+    </div>
   );
 };
 

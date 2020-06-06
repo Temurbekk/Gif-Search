8c43bbf1cd9c7dd983e76e7fdeba677ca49e0979
Temurbek Sabirov <prodota150@gmail.com>
2020-06-05 22:16:59 -0400

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
+
 import Search from "./Components/Search";
 import GifTrending from "./Components/GifTrending";
+
 const App = () => {
   const [searching, setSearching] = useState(true);
   return (
     <div className="container box">
       {!searching ? (
-        <div className="">
+        <div>
           <div className="button" onClick={() => setSearching(true)}>
             Search
           </div>
           <GifTrending />
         </div>
       ) : (
-        <div className="">
+        <div>
           <div className="button" onClick={() => setSearching(false)}>
             Trending
           </div>
